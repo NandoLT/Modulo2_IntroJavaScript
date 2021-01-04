538353043204799652648837bf0575ee40446ed0
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2021-01-04 23:36:18 +0100

    Creación de archivo makingGroups.js, dado un array de equipos hace los grupos de A hasta H

---
+++ makingGroups.js
@@ -0,0 +1,32 @@
+
+const groups = {
+    A: [],
+    B: [],
+    C: [],
+    D: [],
+    E: [],
+    F: [],
+    G: [],
+    H: []
+}
+let start= 0
+let end = 4
+
+function shuffleTeams(teams) {
+    let shuffTeams = teams.sort(() => Math.random() - 0.5) 
+    return shuffTeams
+}
+
+export default function makingGroups(teams) {
+    const teamsCopy = shuffleTeams(teams)
+    for(let key in groups) {
+        for(let i = 0; i < teamsCopy.length / 4; i++) {
+            groups[key] = teamsCopy.slice(start, end)
+        }
+        start = end
+        end += 4
+    }
+    return groups
+}
+
+
