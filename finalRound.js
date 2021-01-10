10d7162d3fb1803d4429c35d9a0610b6c82b6a7f
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2021-01-10 23:49:48 +0100

    Se crea el fichero que almacenará las funciones para disputar las fases de la ronda final

---
+++ finalRound.js
@@ -0,0 +1,13 @@
+/**
+ * Equipos que han pasado la fase de grupos 
+ * juegan la fase final con:
+ * Octavos de final
+ * Cuartos de final
+ * Semifinal
+ * Final
+ * Los puestos a destcar serán:
+ * Primero y Segundo (equipos que juegan la final)
+ * Tercer y Cuarto Puesto (a disputar entre los perdedores de la semifinal)
+ */
+
+ 
